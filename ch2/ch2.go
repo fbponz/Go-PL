27c617328890e590980dc0335763c1286bae59ae
fbponz <fbponz@protonmail.ch>
2025-12-30 08:17:40 +0100

    Exercises from Chapter 2

--- ch2/ch2.go
+++ ch2/ch2.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello, %s!\n", "world")
+	var i int = 20
+	var f float32 = float32(i)
+	const ValueI = 20
+
+	fmt.Println(i)
+	fmt.Println(f)
+
+	var b byte = 255
+	var smallI int32 = 2147483647
+	var bigI uint64 = 18446744073709551615
+
+	b = b + 1
+	smallI = smallI + 1
+	bigI = bigI + 1
+
+	fmt.Println(b)
+	fmt.Println(smallI)
+	fmt.Println(bigI)
+
+	fmt.Println("These three variables has overflow")
 }
